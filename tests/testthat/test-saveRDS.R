a92741ac550f6ac51e5d6fbc21629f7c762e7a5e
RobertZK <technoguyrob@gmail.com>
2015-05-15 14:28:14 -0500

    Failing test for non-native R object.

--- tests/testthat/test-saveRDS.R
+++ tests/testthat/test-saveRDS.R
@@ -7,5 +7,17 @@ describe("Writing a vanilla object", {
     saveRDS(native_obj, file)
     expect_identical(native_obj, readRDS(file))
   })
+})
 
+describe("Writing a non-vanilla object", {
+  test_that("the base saveRDS writes an object with the write method applied", {
+    file <- tempfile()
+    nonnative_obj <- list(x = "pointer")
+    attr(nonnative_obj, "RDS2.serialize") <- list(
+      read  = function(obj) { obj$y <- rawToChar(obj$y); obj },
+      write = function(obj) { obj$y <- charToRaw(obj$y); obj }
+    )
+    saveRDS(nonnative_obj, file)
+    expect_identical(list(x = charToRaw("pointer")), base::readRDS(file))
+  })
 })
